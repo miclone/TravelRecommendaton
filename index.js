fd9f450dd30f3e48d4acd5a512965e2d4f261a7b
miclone <miclone.research@gmail.com>
2024-10-20 23:23:52 +1000

    end of day checkin

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const search = document.querySelector("#search");
 const search_results = document.querySelector("#search-results");
 const noresultfound = document.querySelector("#noresultfound");
 const search_text = document.querySelector("#search-text");
+const arrows = document.querySelector("#arrows");
 
 search_text.addEventListener("keydown", (e) => {
     if (e.key == "Enter") {
-        if (search_text.value != "") {
-
+        search_results.style.display = "none";
+        arrows.style.display = "none";
+        if (search_text.value == "all") {
+            search_results.style.display = "grid";
+            arrows.style.display = "flex";
         }
     }
 });
 
- 
