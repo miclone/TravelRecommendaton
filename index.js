370ca602e533b472d152d3a032cbe6798cba564b
Michael Tan <miclone.research@gmail.com>
2024-10-29 14:24:12 +1000

    Update index.js

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ const result3_pic = document.querySelector("#result3-pic");
 const pic3_caption = document.querySelector("#pic3-caption");
 const pic3_description = document.querySelector("#pic3-description");
 
+const searchedDestinationsData = "searchedDestinations";
+const searchedDestinationsIndexData = "searchedDestinationsIndex";
 
 let countryDestinations = [];
 let templesDestinations = [];
@@ -117,8 +119,73 @@ function setImage3(destination) {
     pic3_description.textContent = destination.description;
 }
 
+function saveSearchedResultsSessionData() {
+    let sessionJsonString = JSON.stringify(searchedDestinations);
+    sessionStorage.setItem(searchedDestinationsData, sessionJsonString);
+}
+
+function loadSearchedResultsSessionData() {
+    searchedDestinations.length = 0;
+    let sessionData = sessionStorage.getItem(searchedDestinationsData);
+    if (sessionData) {
+        let data = JSON.parse(sessionData);
+        Array.from(data).forEach(place => {
+            let locationType = place["locationType"];
+            let locationName = place["locationName"];
+            let localName = place["localName"];
+            let imageUrl = place["imageUrl"];
+            let description = place["description"];
+            let dest = new destination(locationType, locationName, localName, imageUrl, description);
+            searchedDestinations.push(dest);
+        });
+    }
+}
+
+function saveDisplayIndexesSessionData() {
+    sessionStorage.setItem(searchedDestinationsIndexData, indexes.toString());
+}
+
+function loadDisplayIndexesSessionData() {
+    let sessionData = sessionStorage.getItem(searchedDestinationsIndexData);
+    if (sessionData) {
+        let sessionIndex = sessionData.split(",");
+        indexes[0] = parseInt(sessionIndex[0]);
+        indexes[1] = parseInt(sessionIndex[1]);
+        indexes[2] = parseInt(sessionIndex[2]);
+    }
+}
+
+function displaySearchedResults() {
+    let i = 0;
+    let c = 0;
+    Array.from(searchedDestinations).forEach(destination => {
+        if ((c < 3) && (i >= indexes[0]) && (i <= indexes[2]) && (i < searchedDestinations.length)) {
+            switch (c) {
+                case 0:
+                    result1.style.display = "block";
+                    setImage1(destination);
+                    break;
+                case 1:
+                    result2.style.display = "block";
+                    setImage2(destination);
+                    break;
+                case 2:
+                    result3.style.display = "block";
+                    setImage3(destination);
+                    break;
+            }
+            c++;
+        };
+        i++;
+    });
+    search_results.style.display = "grid";
+    if (searchedDestinations.length > 3) {
+        arrows.style.display = "flex";
+    }
+}
 
 function searchDesiredDestinations() {
+    sessionStorage.clear();
     dreamtravelcenter.style.display = "none";
     search_results.style.display = "none";
     arrows.style.display = "none";
@@ -146,32 +213,12 @@ function searchDesiredDestinations() {
         addFilteredPlanetDestinations();
     }
     if (searchedDestinations.length > 0) {
-        let i = 0;
-        Array.from(searchedDestinations).forEach(destination => {
-            if ((i < 3) && (i < searchedDestinations.length)) {
-                switch (i) {
-                    case 0:
-                        result1.style.display = "block";
-                        setImage1(destination);
-                        break;
-                    case 1:
-                        result2.style.display = "block";
-                        setImage2(destination);
-                        break;
-                    case 2:
-                        result3.style.display = "block";
-                        setImage3(destination);
-                        break;
-                }
-                i++;
-            }
-        });
-        search_results.style.display = "grid";
-        if (searchedDestinations.length > 3) {
-            arrows.style.display = "flex";
-        }
+        displaySearchedResults();
+        saveSearchedResultsSessionData();
+        saveDisplayIndexesSessionData();
     } else {
         noresultfound.style.display = "flex";
+        sessionStorage.removeItem("searchedDestinations");
     }
 }
 
@@ -199,8 +246,8 @@ function moveImages() {
     setImage1(searchedDestinations[indexes[0]]);
     setImage2(searchedDestinations[indexes[1]]);
     setImage3(searchedDestinations[indexes[2]]);
+    saveDisplayIndexesSessionData();
 }
-
 
 function lefttArrowClicked() {
     changeIndexValue(0, indexes[0] - 1);
@@ -227,6 +274,11 @@ function loadApiJson() {
     Array.from(json.planets).forEach(destination => {
         planetsDestinations.push(destination);
     });
+    loadSearchedResultsSessionData();
+    loadDisplayIndexesSessionData();
+    if (searchedDestinations.length > 0) {
+        displaySearchedResults();
+    }
 }
 
 var req = new XMLHttpRequest();
